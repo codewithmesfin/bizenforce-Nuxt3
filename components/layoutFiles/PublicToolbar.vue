fc2423776a5957d8a81669968870c22067c8575d
System Administrator <sciemesfin55@gmail.com>
2022-11-18 14:24:20 +0300

    save changes

--- components/layoutFiles/PublicToolbar.vue
+++ components/layoutFiles/PublicToolbar.vue
@@ -53,7 +53,7 @@
                 </DisclosureButton>
                 <DisclosureButton href="/support"
                     className="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-blue-600 hover:bg-gray-100">
-                 Get Support
+                 Contact us now
                 </DisclosureButton>
             </div>
         </DisclosurePanel>
